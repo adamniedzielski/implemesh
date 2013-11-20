26b485abb1e741a954f490eadfe9579a104bbed5
Adam Niedzielski <adamsunday@gmail.com>
2013-11-20 20:33:55 +0100

    Refactor Fourier specs

--- spec/operations/dft_spec.rb
+++ spec/operations/dft_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe Implemesh::Operations::DFT do
   
   let(:dft) { Implemesh::Operations::DFT }
-  let(:before_fourier) { [[5, 0], [3, 0], [1, 0], [2, 0], [5, 0], [6, 0], [1, 0], [9, 0]] }
-  let(:after_fourier) { [[32, 0], [2.8, 7.1], [8, 2], [-2.8, 7.1], [-8, 0], [-2.8, -7.1], [8, -2], [2.8, -7.1]] }
 
   before do
     @signal = Implemesh::Signal.new
@@ -15,18 +13,18 @@ describe Implemesh::Operations::DFT do
   context "forward transformation" do
 
     it "gives correct results" do
-      @signal.samples = before_fourier
+      @signal.samples = samples_before_fourier
       result = dft.perform(@signal)
-      expect(result.samples.flatten).to be_similar_array_to after_fourier.flatten
+      expect(result.samples.flatten).to be_similar_array_to samples_after_fourier.flatten
     end
   end
 
   context "inverse transformation" do
 
     it "gives correct results" do
-      @signal.samples = after_fourier
+      @signal.samples = samples_after_fourier
       result = dft.perform(@signal, :inverse => true)
-      expect(result.samples.flatten).to be_similar_array_to before_fourier.flatten
+      expect(result.samples.flatten).to be_similar_array_to samples_before_fourier.flatten
     end
   end
 end
